a2f92d80935ad004e1096730916dab5f06b6d591
Zane Chan <dung@Zanes-MacBook-Air.local>
2024-02-04 14:20:17 -0500

    Added initial files for the project

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+# Keep Chrome Browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.google.ca")
+
