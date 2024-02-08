281689c01596049bb6c54fee8edb5113a1bbe79c
Zane Chan <dung@Zanes-MacBook-Air.local>
2024-02-08 14:02:21 -0500

    Added helper functions, main function for the bot to click cookie for 5 minutes and buy best upgrades every 5 seconds

--- main.py
+++ main.py
@@ -15,11 +15,68 @@ driver.get(URL)
 # Find cookie element for clicking
 cookie = driver.find_element(By.ID, value="cookie")
 
-# find upgrades/items in the store
-store_items = [item for item in driver.find_elements(By.CSS_SELECTOR, value="#rightPanel #store div")]
+# Find upgrades/items in the store
+store_items = [item for item in driver.find_elements(By.CSS_SELECTOR, value="#rightPanel #store div") if item.text != ""]
 # for item in store_items:
 #     print(item.text)
 
+# Find items id
+items_ids = [item.get_attribute("id") for item in store_items if item.text != ""]
 
+# Helper Functions:
+# Find price of the item function
+def get_price(item_value):
+    item_price = item_value.find_element(By.TAG_NAME, value="b")
+    return int(item_price.text.split("-")[1].replace(",", ""))
+
+
+# Find all prices of the items
+def all_prices():
+    store = [int(item.text.split("-")[1].replace(",", "")) for item in
+             driver.find_elements(By.CSS_SELECTOR, value="#store b") if item.text != "" ]
+    return store
+
+
+# Get money
+def get_money():
+    money = int(driver.find_element(By.ID, value="money").text.strip().replace(",", ""))
+    return money
+
+
+# 5 minutes from now
+five_mins = time.time() + 60 * 5
+# 5 seconds
+timeout = time.time() + 5
+
+# for five minutes
+while True:
+    cookie.click()
+    # Every 5 secs
+    if time.time() > timeout:
+        # get all prices
+        items_prices = all_prices()
+        # get current amount of money
+        available_money = get_money()
+
+        # find the available items to buy
+        all_items = {}
+        for n in range(len(items_prices)):
+            if available_money > items_prices[n]:
+                all_items[items_prices[n]] = items_ids[n]
+
+        # find and click the most expensive item from the list
+        max_price = max(all_items)
+        buy_item_id = all_items[max_price]
+        buy_item = driver.find_element(By.ID, value=buy_item_id)
+        buy_item.click()
+
+        # Add another 5 seconds until the next check
+        timeout = time.time() + 5
+
+    # After five minutes
+    if time.time() > five_mins:
+        cookies_per_sec = driver.find_element(By.ID, value="cps")
+        print(cookies_per_sec.text)
+        break
 
 # driver.quit()
